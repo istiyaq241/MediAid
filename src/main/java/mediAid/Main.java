7d9b55196622a304ea40c1c8e5294772d4c5050a
Istiyaq Ahmed <ahmed241-15-020@diu.edu.bd>
2025-07-26 13:13:49 +0600

    Updated Main

--- src/main/java/mediAid/Main.java
+++ src/main/java/mediAid/Main.java
@@ -1,31 +1,27 @@
-// Line 1
-package mediAid; // Line 2
+package mediAid;
 
-// Line 3
-import javafx.application.Application;       // Line 4
-import javafx.scene.Scene;                   // Line 5
-import javafx.scene.control.Label;           // Line 6
-import javafx.scene.layout.VBox;             // Line 7
-import javafx.stage.Stage;                   // Line 8
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-// Line 9
-public class Main extends Application {      // Line 10
+public class Main extends Application {
 
-    @Override                                // Line 11
-    public void start(Stage primaryStage) {  // Line 12
-        // Temporary homepage UI              // Line 13
-        Label welcome = new Label("Welcome to MediAid"); // Line 14
+    @Override
+    public void start(Stage primaryStage) {
+        Label welcome = new Label("Welcome to MediAid");
 
-        VBox root = new VBox(10);            // Line 15
-        root.getChildren().add(welcome);     // Line 16
+        VBox root = new VBox(10);
+        root.getChildren().add(welcome);
 
-        Scene scene = new Scene(root, 400, 200);         // Line 17
-        primaryStage.setTitle("MediAid System");         // Line 18
-        primaryStage.setScene(scene);                   // Line 19
-        primaryStage.show();                             // Line 20
-    }                                                    // Line 21
+        Scene scene = new Scene(root, 400, 200);
+        primaryStage.setTitle("MediAid System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
-    public static void main(String[] args) {             // Line 22
-        launch(args);                                    // Line 23
-    }                                                    // Line 24
-}                                                        // Line 25
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
